e5a34f716e2f9ace6a9142496575f0d82d0b8fbf
leanne <leanne@quadzilla.local>
2016-02-21 11:25:40 -0600

    corrected layout; implemented image picker

--- MemeMe_v1.0/MemeMeViewController.swift
+++ MemeMe_v1.0/MemeMeViewController.swift
@@ -10,7 +10,12 @@ import UIKit
 
 class MemeMeViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
 
+	// MARK: - Outlets
+	
 	@IBOutlet weak var memeImage: UIImageView!
+	
+	
+	// MARK: - View Controller Overrides
 	
 	override func viewDidLoad() {
 		super.viewDidLoad()
@@ -22,6 +27,28 @@ class MemeMeViewController: UIViewController, UIImagePickerControllerDelegate, U
 		// Dispose of any resources that can be recreated.
 	}
 
+	
+	// MARK: - Actions
+	
+	@IBAction func pickAMemeImage(sender: UIBarButtonItem) {
+		let imagePickerController = UIImagePickerController()
+		imagePickerController.delegate = self
+		
+		presentViewController(imagePickerController, animated: true, completion: nil)
+	}
+	
+	
+	// MARK: - Image Picker Delegate Methods
+	
+	func imagePickerController(picker: UIImagePickerController, didFinishPickingImage image: UIImage, editingInfo: [String : AnyObject]?) {
+		memeImage.image = image
+		
+		dismissViewControllerAnimated(true, completion: nil)
+	}
+	
+	func imagePickerControllerDidCancel(picker: UIImagePickerController) {
+		dismissViewControllerAnimated(true, completion: nil)
+	}
 
 }
 
