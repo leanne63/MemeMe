33ddca73bb05d66d33b2a4259c6a07e383698a51
leanne63 <leanne63@users.noreply.github.com>
2016-02-25 16:07:05 -0600

    added unit and UI testing frameworks with default test values

--- MemeMeUITests/MemeMeUITests.swift
+++ MemeMeUITests/MemeMeUITests.swift
@@ -31,6 +31,10 @@ class MemeMeUITests: XCTestCase {
     func testExample() {
         // Use recording to get started writing UI tests.
         // Use XCTAssert and related functions to verify your tests produce the correct results.
-    }
+		
+		let app = XCUIApplication()
+		
+		XCTAssertNotNil(app.toolbars.buttons["Album"])
+	}
     
 }
