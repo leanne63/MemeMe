b1bdeb6afe8987d97634057f109cc4a04829f740
leanne63 <leanne63@users.noreply.github.com>
2016-03-10 11:51:23 -0600

    fixed empty table message so appears only when table is empty

--- MemeMe/TableViewController.swift
+++ MemeMe/TableViewController.swift
@@ -36,25 +36,30 @@ class TableViewController: UITableViewController {
 		let numRows = savedMemes.count
 		
 		// code modified from:
-		// iOS Programming 101: Implementing Pull-to-Refresh and Handling Empty Table,
+		// iOS Programming 101: Implementing Pull-to-Refresh and Handling Empty Table
 		//	Simon Ng, 11 July 2014
 		//	http://www.appcoda.com/pull-to-refresh-uitableview-empty/
 		if numRows > 0 {
-			return numRows
+			if tableView.backgroundView != nil {
+				tableView.backgroundView = nil
+				tableView.separatorStyle = .SingleLine
+			}
 		}
 		else {
-			let emptyMessageLabel = UILabel(frame: CGRectMake(0, 0, view.bounds.size.width, view.bounds.size.height))
-			emptyMessageLabel.text = "No memes sent yet!\nPress + to create a new meme\nand share it."
-			emptyMessageLabel.numberOfLines = 0
-			emptyMessageLabel.font = UIFont(name: "Palatino-Italic", size: 20)
-			emptyMessageLabel.textAlignment = .Center
-			emptyMessageLabel.sizeToFit()
-			
-			tableView.backgroundView = emptyMessageLabel
-			tableView.separatorStyle = .None
-			
-			return 0
+			if tableView.backgroundView == nil {
+				let emptyMessageLabel = UILabel(frame: CGRectMake(0, 0, view.bounds.size.width, view.bounds.size.height))
+				emptyMessageLabel.text = "No memes sent yet!\nPress + to create a new meme\nand share it."
+				emptyMessageLabel.numberOfLines = 0
+				emptyMessageLabel.font = UIFont(name: "Palatino-Italic", size: 20)
+				emptyMessageLabel.textAlignment = .Center
+				emptyMessageLabel.sizeToFit()
+				
+				tableView.backgroundView = emptyMessageLabel
+				tableView.separatorStyle = .None
+			}
 		}
+		
+		return numRows
     }
 
     override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
@@ -63,12 +68,59 @@ class TableViewController: UITableViewController {
 		
 		let currentMeme = savedMemes[indexPath.row]
 		
-		let imageView = cell.viewWithTag(1) as! UIImageView
-		let label = cell.viewWithTag(2) as! UILabel
+		let cellImageView = cell.viewWithTag(1) as! UIImageView
+		cellImageView.image = currentMeme.memedImage
 		
-		imageView.image = currentMeme.memedImage
+		// set up text for label
+		// BABY HEDGEHOG SAYS - I NEED MY SPACE
+		// BABY HED...MY SPACE
 		
-		label.text = currentMeme.topMemeText + "..." + currentMeme.bottomMemeText
+		// COW BE LIKE - HANG TEN, BRAH
+		// COW BE LI...TEN, BRAH
+		
+		// TEST MEME - YOU'RE DOING IT WRONG
+		// TEST MEM...IT WRONG
+		
+		// SET UP - HILARIOUS PUNCHLINE
+		// SET UP...HI...UNCHLINE
+		
+		let topText = currentMeme.topMemeText
+		let bottomText = currentMeme.bottomMemeText
+		let ellipsis = "..."
+		
+		// get applicable character counts
+		let numTopChars = topText.characters.count
+		let numBottomChars = bottomText.characters.count
+		let numEllipsisChars = ellipsis.characters.count
+		
+		let totalNumChars = numTopChars + numBottomChars
+		let totalWithEllipsis = totalNumChars + numEllipsisChars
+		
+		let halfPoint = totalNumChars / 2
+		
+		// now, setup appropriate text for label
+		var labelText = ""
+		if numTopChars <= halfPoint {
+			labelText += topText
+		}
+		else {
+			// calculate num chars to show before ellipsis
+			labelText += "x"
+		}
+		
+		labelText += ellipsis
+		
+		if numBottomChars <= halfPoint {
+			labelText += bottomText
+		}
+		else {
+			// calculate num chars to show after ellipsis
+			labelText += "x"
+		}
+		
+		
+		let cellLabel = cell.viewWithTag(2) as! UILabel
+		cellLabel.text = labelText
 
         return cell
     }
